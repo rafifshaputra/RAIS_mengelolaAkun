f20dddae16bc234f4bfc8521881e0cf5d12b41e4
Rafif Shaputra <rafifshaputra25@gmail.com>
2019-03-24 09:22:01 +0700

    Add SideNav and Table Vendor

---
+++ database/migrations/2019_03_24_012157_create_vendors_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateVendorsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('vendors', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('namaVendor',128);
+            $table->string('jenisVendor',128);
+            $table->string('alamat',128);
+            $table->string('kontak',128);
+            $table->string('informasi',128);
+            $table->string('biaya',128);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('vendors');
+    }
+}
--- resources/views/layouts/homeMaster.blade.php
+++ resources/views/layouts/homeMaster.blade.php
@@ -1,13 +1,15 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8">
+
     <title> @yield('title')</title>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"></script>
+
     <style>
     body{
         padding:10px;
@@ -15,6 +17,9 @@
     }
 
    nav{
+        margin-left: 160px; /* Same as the width of the sidenav */
+        /* Increased text to enable scrolling */
+        padding: 0px 10px;
         padding-left:10px;
         padding-right:10px;
     }
@@ -23,11 +28,69 @@
         padding-top:15px;
     }
 
+    .sidenav {
+        height: 100%;
+        width: 160px;
+        position: fixed;
+        z-index: 1;
+        top: 0;
+        left: 0;
+        background-color: #111;
+        overflow-x: hidden;
+        padding-top: 20px;
+    }
+
+    .sidenav a {
+        padding: 6px 8px 6px 16px;
+        text-decoration: none;
+        font-size: 18px;
+        color: #818181;
+        display: block;
+    }
+
+    .sidenav a:hover {
+        color: #f1f1f1;
+    }
+
+    .main {
+        margin-left: 160px; /* Same as the width of the sidenav */
+        font-size: 12px; /* Increased text to enable scrolling */
+        padding: 0px 10px;
+    }
+
+    @media screen and (max-height: 450px) {
+    .sidenav {padding-top: 15px;}
+    .sidenav a {font-size: 18px;}
+    }
+
+
     </style>
 </head>
 
 <body>
+    <div class="sidenav">
+            <img src="ra1.png" alt="" id = "gambar-rais" class = "center">
+            <br><br>
+            <div>
+                <img src="ra1.png" alt="" class = "gambar-rais-kiri">
+                <a href="#about">Beranda</a>
+            </div><br>
+            <div>
+                <img src="ra1.png" alt="" class = "gambar-rais-kiri">
+                <a href="#services">Info</a>
+            </div><br>
+            <div>
+                <img src="ra1.png" alt="" class = "gambar-rais-kiri">
+                <a href="#clients">Tentang</a>
+            </div><br>
+            <div>
+                <img src="ra1.png" alt="" class = "gambar-rais-kiri">
+                <a href="#contact">Pengaturan Akun</a>
+            </div>
+    </div>
+
     <header>
+
         <nav>
             <a href="#"><span style="font-size:28px;color:#2A2E43;">Dashboard</span></a>
             <span class="right-nav" style=" align:120px;float:right;font-size:15px;padding-top:2px;">
@@ -40,8 +103,14 @@
         </nav>
     <span> <hr style="border-top: 2px solid #ADADAD;opacity:0.5;"></span>
     </header>
+
     <br>
-    @yield('content')
+
+
+
+    <div class="main">
+        @yield('content')
+    </div>
 
     <br>
     <footer>
@@ -49,6 +118,7 @@
             &copy; Developed by RAIS Developer Team
         </p>
     </footer>
+
 
 </body>
 </html>
